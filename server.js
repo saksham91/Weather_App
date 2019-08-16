88bda0e877088a8c342bb01dee8f6bb1994b2131
saksham91 <38701227+saksham91@users.noreply.github.com>
2019-08-16 18:44:06 -0400

    API Key problem solved

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
 
-//const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 const axios = require('axios');
 const express = require('express');
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
-    const url = `https://api.darksky.net/forecast/DARKSKY_API_KEY/${req.body.latitude},${req.body.longitude}?units=auto`;
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`;
     axios({
         url: url,
         responseType: 'json'
